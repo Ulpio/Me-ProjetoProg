06ea03783f9e975d0072d8d03910bd3be6cdabfe
ulpio <ulpionetto0@gmail.com>
2022-10-22 16:15:42 -0300

    Primeiro Commit

--- ME-1UP/src/Pessoa.java
+++ ME-1UP/src/Pessoa.java
@@ -1,4 +1,21 @@
 public class Pessoa {
     private String nome;
     private String cpf;
+    public Pessoa(String nome,String cpf){
+        this.nome = nome;
+        this.cpf = cpf;
+    }
+    public String getNome(){
+        return this.nome;
+    }
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+    public String getCpf(){
+        return this.nome;
+    }
+    public void setCpf(String cpf){
+        this.cpf = cpf;
+    }
+
 }
